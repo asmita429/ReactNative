cdac3c5b292bd7b95890bc110fae05e50360027c
Asmita Thapa <asmitathapa429@gmail.com>
2024-05-12 17:09:52 +0545

    first typescript file

--- App.tsx
+++ App.tsx
@@ -1,118 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import {Text, View, Button,} from 'react-native';
+import CompanyData from './components/CompanyData';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App= () => {
+  // state data cannot be shared, instead we share props.
+  let data = 10;
+  const fruit = (val: string) => {
+    console.warn("function called", val)
+    data = 20;
+    // console.warn(data);
+  }
+  const [name, setName] = useState("Asmita");
+  function testName() {
+    setName("Subash")
+  }
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+   <View>
+    <Text style={{fontSize:30}}>Hello React native</Text>
+    <Text style={{fontSize:20}}>Learn</Text>
+    <Text style={{fontSize:20}}>Code</Text>
+
+    {/* <Button title='On Press' color={'green'} onPress={fruit}/> */}
+    <Button title='On Press 2' color={'blue'} onPress={() => fruit("asmita")}/>
+    <Text style={{fontSize:20}}>{data}</Text>
+
+    <Text style={{fontSize: 20}}>{name}</Text>
+    <Button title='update Name' onPress={testName}/>
+
+    <CompanyData />
+    <UserData />
+
+    
+   </View>
+  );
+}
+
+const UserData = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 10}}>Name: Asmita </Text>
+      <Text style={{fontSize: 10}}>Age: 24 </Text>
+      <Text style={{fontSize: 10}}>Blood Group: A+</Text>
+      <Text style={{fontSize: 10}}>Email: name@gmail.com</Text>
     </View>
   );
-}
+};
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ components/CompanyData.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const CompanyData = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Name: Atlassian </Text>
+      <Text style={{fontSize: 30}}>Age: 24 </Text>
+      <Text style={{fontSize: 30}}>Email: name@gmail.com</Text>
+    </View>
+  );
+};
+
+export default CompanyData;
